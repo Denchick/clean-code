a4554c0dae89d82806dbe50bedc8ff1a497a8013
Volkov Denis <denchick1997@mail.com>
2017-11-19 23:08:27 +0500

    Добавил возможность разметки файла

--- Markdown/Program.cs
+++ Markdown/Program.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Markdown.MarkupRules;
 
 namespace Markdown
 {
 	class Program
 	{
-		//давай научимся принимать любой текст и выдывать Markdown
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
+			var textFromFile = File.ReadAllText(@"input.txt");
+			var markupRules = new List<IMarkupRule>() {new Bold(), new Cursive(), new Header()};
+			var md = new Md(markupRules);
+			var result = md.RenderToHtml(textFromFile);
+			
+			using (var sw = new StreamWriter(@"output.txt"))
+			{
+				sw.WriteLine(result);
+			}
+			
 		}
 	}
 }
